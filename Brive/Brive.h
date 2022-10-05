fad920c82a5c4d96d36aa76daa389e204ec81599
gosha <gosha.titov@icloud.com>
2022-10-05 15:32:02 +0300

    Feat: create Brive project.

---
+++ Brive/Brive.h
@@ -0,0 +1,18 @@
+//
+//  Brive.h
+//  Brive
+//
+//  Created by Gosha on 05.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Brive.
+FOUNDATION_EXPORT double BriveVersionNumber;
+
+//! Project version string for Brive.
+FOUNDATION_EXPORT const unsigned char BriveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Brive/PublicHeader.h>
+
+
