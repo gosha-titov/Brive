70a7cd646cee320415999b7eeddf7318b93adb35
gosha <gosha.titov@icloud.com>
2022-10-07 17:51:15 +0300

    Feat: add set(view:) method.

--- Brive/Architecture/Module/Router/Launchable.swift
+++ Brive/Architecture/Module/Router/Launchable.swift
@@ -4,6 +4,9 @@ import UIKit
 /// A type that the root router should conform to.
 public protocol Launchable where Self: DefaultRouter {
     
+    /// Sets the router's view that will be shown.
+    func set(view: UIViewController?)-> Void
+    
     /// Launches the router from the given window.
     func launch(from window: UIWindow) -> Void
     
@@ -12,10 +15,18 @@ public protocol Launchable where Self: DefaultRouter {
 
 public extension Launchable {
     
-    func launch(from window: UIWindow) -> Void {
+    func set(view: UIViewController?) -> Void {
+        self.view = view
         if let self = self as? NavigationControllable {
             self.rootController = UINavigationController()
-            view.executeSafely { self.rootController!.pushViewController($0) }
+            view.executeSafely {
+                self.rootController!.pushViewController($0)
+            }
+        }
+    }
+    
+    func launch(from window: UIWindow) -> Void {
+        if let self = self as? NavigationControllable {
             window.rootViewController = self.rootController
         } else if let self = self as? TabBarControllable {
             window.rootViewController = self.rootController
