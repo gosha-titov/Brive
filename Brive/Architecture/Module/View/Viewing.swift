245f23f0f7054228216242e833eb9482c32ba95b
gosha <gosha.titov@icloud.com>
2022-10-17 18:55:42 +0300

    Feat: add empty implementation for showLoading() and hideLoading() methods. Docs: update descriptions.

--- Brive/Architecture/Module/View/Viewing.swift
+++ Brive/Architecture/Module/View/Viewing.swift
@@ -1,10 +1,19 @@
 /// An interface for the interactor trough which it communicates with this view.
 public protocol Viewing: AnyObject {
     
-    /// Shows loading indicator.
+    /// Shows loading.
     func showLoading() -> Void
     
-    /// Hides loading indicator.
+    /// Hides loading.
     func hideLoading() -> Void
+    
+}
+
+
+public extension Viewing {
+    
+    func showLoading() -> Void {}
+    
+    func hideLoading() -> Void {}
     
 }
